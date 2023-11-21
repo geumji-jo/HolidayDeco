303f62af090514b8b11a595f5630cdcffb0a0cf2
bbooom2 <bbooom2>
2023-11-21 21:36:07 +0900

    2023-11-21

---
+++ src/main/java/com/hdd/hdeco/HdecoApplication.java
@@ -0,0 +1,13 @@
+package com.hdd.hdeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdecoApplication.class, args);
+	}
+
+}
