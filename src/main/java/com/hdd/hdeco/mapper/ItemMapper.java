42c51147ff64019741f0541bd43f4119e714d6d3
bbooom2 <bbooom2>
2023-11-23 23:14:31 +0900

    2023-11-23-bbooom2-item조회일부

---
+++ src/main/java/com/hdd/hdeco/mapper/ItemMapper.java
@@ -0,0 +1,9 @@
+package com.hdd.hdeco.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class ItemMapper {
+	//public int getItemCount();
+
+}
---
+++ src/main/java/com/hdd/hdeco/service/ItemService.java
@@ -0,0 +1,5 @@
+package com.hdd.hdeco.service;
+
+public interface ItemService {
+
+}
