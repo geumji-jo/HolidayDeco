b3349cada67f458ec119352749ed4477fad8d2e4
bbooom2 <bbooom2>
2023-12-18 22:03:43 +0900

    2023-12-18-bbooom2-index수정 및 footer추가

--- src/main/java/com/hdd/hdeco/controller/ItemOrderController.java
+++ src/main/java/com/hdd/hdeco/controller/ItemOrderController.java
@@ -1,5 +1,6 @@
 package com.hdd.hdeco.controller;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,8 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hdd.hdeco.domain.ItemOrderDTO;
@@ -22,7 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/order")
 @Controller
 public class ItemOrderController {
-	
+
 	private final ItemOrderService itemOrderService;
 	private final CartService cartService;
 
@@ -32,22 +36,49 @@ public class ItemOrderController {
 		model.addAttribute("cartList", cartService.getCartList(request, response));
 		return "order/orderDetails";
 	}
-	
+
 	@PostMapping("/orderSelect.do")
 	public String orderSelect(HttpServletRequest request, Model model) {
 		model.addAttribute("orderUser", itemOrderService.getUserInfo(request));
 		model.addAttribute("cartList", itemOrderService.getSelectItemList(request));
 		return "order/orderDetails";
-	}	
-	
-	
+	}
+
 	// 결제하기 버튼 누르면 실행
 	@ResponseBody
-	@PostMapping(value="/insertOrder.do", produces="application/json") 
-	public Map<String, Object> insertOrder(ItemOrderDTO itemOrderDTO, Model model){
+	@PostMapping(value = "/insertOrder.do", produces = "application/json")
+	public Map<String, Object> insertOrder(ItemOrderDTO itemOrderDTO, Model model) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("order", itemOrderService.insertOrder(itemOrderDTO));
 		return map;
 	}
-	
+
+	// 결제완료
+	@RequestMapping(value = "/paymentFinish.html", method = { RequestMethod.GET, RequestMethod.POST })
+	public String paymentFinish(ItemOrderDTO itemOrderDTO, Model model) {
+
+		// 주문번호 만들기 (랜덤번호 + 주문년월일)
+		String randomNum = Math.random() + "";
+		randomNum += LocalDate.now() + "";
+
+		// 소수 부분만 남기고, - 없앤 후, 2023 대신 -23으로
+		int dotIndex = randomNum.indexOf(".");
+		String noDotRandomNum = randomNum.substring(dotIndex + 1);
+		noDotRandomNum = noDotRandomNum.replace("-", "");
+		String changeNum = noDotRandomNum.replace("20", "-");
+		model.addAttribute("orderNum", changeNum);
+
+		model.addAttribute("map", itemOrderService.insertOrder(itemOrderDTO));
+
+		return "order/paymentFinish";
+	}
+
+	/*
+	 * 결제 취소
+	 * 
+	 * @GetMapping("/paymentFail.do") public String paymentFail(HttpServletRequest
+	 * request) { itemOrderService.deleteOrder(request); return
+	 * "redirect:/item/classDetail.do?classNo=" + request.getParameter("classNo"); }
+	 */
+
 }
