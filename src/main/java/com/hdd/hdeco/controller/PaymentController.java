f765d14b0473e18648de3c1861c47d74528bbaaf
geumji <stdal0330@gmail.com>
2024-01-08 15:08:12 +0900

    2024-01-08-geumji-orderDetail수정

---
+++ src/main/java/com/hdd/hdeco/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.hdd.hdeco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/pay")
+@Controller
+public class PaymentController {
+	
+
+  
+  
+};
